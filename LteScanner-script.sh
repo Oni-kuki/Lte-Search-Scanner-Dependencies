de4a94300b4a2adb698fec57038ca41c0bc03655
Oni-kuki <88449481+Oni-kuki@users.noreply.github.com>
2021-10-10 11:52:17 +0200

    Update LteScanner-script.sh

--- LteScanner-script.sh
+++ LteScanner-script.sh
@@ -2,4 +2,4 @@
 apt update && apt upgrade -y && apt install cmake && apt install -y libopenblas-dev libhackrf-dev build-essential libboost-system-dev libboost-thread-dev libboost-program-options-dev libboost-test-dev libfftw3-3 libfftw3-dev liblapack-dev libblas-dev librtlsdr-dev libncurses5-dev libitpp-dev
 
 #installation of LTEsearchScanner
-mkdir /home/tools/ && cd /home/tools && git clone https://github.com/JiaoXianjun/LTE-Cell-Scanner && cd /home/tools/LTE-Cell-Scanner && mkdir build && cd build && cmake ../ -DUSE_HACKRF=1 && make
+mkdir /home/git-sources && cd /home/git-sources && git clone https://github.com/JiaoXianjun/LTE-Cell-Scanner && cd /home/git-sources/LTE-Cell-Scanner && mkdir build && cd build && cmake ../ -DUSE_HACKRF=1 && make
